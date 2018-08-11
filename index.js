27e961bf8d4d4d27bad251223171d8afda15add4
nptravis <nptravis@yahoo.com>
2018-08-11 20:38:17 +0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const app = "I don't do much.";
+
+let token = '4ce19b86dc2c93ad486ec9b065f18aab490a33a4';
+
+fetch('https://api.github.com/user/repos', {
+	headers: {
+		Authorization: `token ${token}`
+	}
+}).
+  then(res => res.json()).
+  then(json => console.log(json))
